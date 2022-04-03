a6952ac4f706952a3a95b2bb39df99caff5b3934
Dünya <53619378+Dunya-8a@users.noreply.github.com>
2022-04-03 17:00:00 +0100

    Set up basic folder structure for routing

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.development || 8080;
+const PORT = process.env.PORT || 8080;
+
+const users = require("./routes/usersRoute");
+const videos = require("./routes/videoRoute");
 
 app.listen(PORT, () => {
 	console.log(`App is listening on port ${PORT}`);
